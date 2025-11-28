e643b66ddb53b604e3032608e50f9383a368b343
Rodrigo1749 <hr.neris22@info.uas.edu.mx>
2025-11-28 09:47:02 -0700

    Se agrega LoginActivity

---
+++ app/src/main/java/com/example/recetariosocial/model/Perfil.java
@@ -0,0 +1,28 @@
+package com.example.recetariosocial.model;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+
+@Entity(tableName = "Perfiles")
+public class Perfil {
+
+        @PrimaryKey(autoGenerate = true)
+        public int idPerfil;
+
+       // @ForeignKey()
+        public int idUsuario;
+
+        @ColumnInfo(name = "correo")
+        public String correo;
+
+        @ColumnInfo(name = "contraseña")
+        public String contraseña;
+
+        @ColumnInfo(name = "biografia")
+        public String biografia;
+
+    }
+
